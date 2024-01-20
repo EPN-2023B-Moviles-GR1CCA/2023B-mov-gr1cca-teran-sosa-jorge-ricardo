be1189af1710d2b5364c3b4d9b59e3a71218c25f
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2024-01-20 01:38:39 -0500

    Proyecto

---
+++ 04-Examenes/app/src/main/java/com/example/examenibricardoteran/MainInterface.kt
@@ -0,0 +1,11 @@
+package com.example.examenibricardoteran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainInterface : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_interface)
+    }
+}
