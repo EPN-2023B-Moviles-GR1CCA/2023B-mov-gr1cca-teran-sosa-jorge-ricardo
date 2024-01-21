a3c2b1389a0341f5bf7783f25906f16d2cc7e2da
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2024-01-21 12:21:50 -0500

    Examen2.0

--- 04-Examenes/app/src/main/java/com/example/examenibricardoteran/MainActivity.kt
+++ 04-Examenes/app/src/main/java/com/example/examenibricardoteran/MainActivity.kt
@@ -4,24 +4,37 @@ import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.example.examenibricardoteran.db.DbHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCrearBD: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Creando DB
+        btnCrearBD = findViewById(R.id.btnCrearBD) // Asigna el botón desde el layout
+
+        btnCrearBD.setOnClickListener {
+            // Cuando se hace clic en el botón, se crea la base de datos
+            crearBaseDeDatos()
+        }
+    }
+
+    private fun crearBaseDeDatos() {
         val dbHelper: DbHelper = DbHelper(this)
         val db: SQLiteDatabase = dbHelper.writableDatabase
         if (db != null) {
-            //Toast.makeText(this, "BASE DE DATOS CREADA", Toast.LENGTH_LONG).show()
+            // La base de datos se creó con éxito
+            Toast.makeText(this, "BASE DE DATOS CREADA", Toast.LENGTH_LONG).show()
+
+            // Luego, navegamos a la actividad Inmobiliaria
+            irActividad(Inmobiliaria::class.java)
         } else {
             Toast.makeText(this, "ERROR AL CREAR LA BD", Toast.LENGTH_LONG).show()
         }
-        // Fin creacion DB
-        irActividad(Inmobiliaria::class.java)
     }
 
     fun irActividad(
